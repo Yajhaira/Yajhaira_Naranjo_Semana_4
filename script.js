b42a76d1536dff5f8350dcfb9097e2de6ea534f4
Yajhaira <yajhi2002@gmail.com>
2022-09-01 22:44:00 -0500

    Añade lista de elemntos en js

--- script.js
+++ script.js
@@ -1,3 +1,86 @@
 const todas = document.getElementById("all");
-const pendientes = document.getElementById("pending");
-const completadas = document.getElementById("completed");
+const listaPendientes = document.getElementById("pending");
+const listaCompletadas = document.getElementById("completed");
+
+let pendientes = [];
+let completadas = [];
+
+const tareas = [
+    {
+        nombre: "Sacar las perritas",
+        estado: false
+    },
+    {
+        nombre: "Preparar clase de la electiva",
+        estado: false
+    },
+    {
+        nombre: "Tarea de web",
+        estado: false
+    },
+    {
+        nombre: "Comprar alimentos de la semana",
+        estado: false
+    },
+    {
+        nombre: "Sacar dinero",
+        estado: false
+    },
+    {
+        nombre: "Pagar plan de datos",
+        estado: false
+    },
+    {
+        nombre: "Hacer envío de dineros",
+        estado: false
+    },
+    {
+        nombre: "Presentación de arte y tecnología",
+        estado: false
+    },
+    {
+        nombre: "Lectura de electiva",
+        estado: false
+    }
+]
+
+
+let dato;
+let c = '';
+
+allTaskListNav.forEach(element => {
+    c += `<li class = "todas">${element.nombre}</li>`;
+});
+
+allTaskList.innerHTML = `<ul>
+${c}
+</ul>`;
+
+const listaElementos = document.getElementsByClassName('todas');
+
+for(let i = 0; i < listaElementos.length; i++) {
+    listaElementos[i].addEventListener("click", () => {
+        pendientes = [];
+        completadas = [];
+        if (listaElementos[i].className === "pendientes") {
+            listaElementos[i].className = "completadas"
+            dato = listaElementos[i].innerHTML;
+        } else {
+            listaElementos[i].className = "pendientes"
+            dato = listaElementos[i].innerHTML;
+        }
+        allTaskList.forEach(element => {
+            if (dato === element.nombre){
+                element.estado = !element.estado
+            }
+            if(!element.estado){
+                pendientes.push(element);
+            } else {
+                completados.push(element);
+            }
+        })
+    })
+}
+
+
+
